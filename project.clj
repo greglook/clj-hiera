9a9ccf5392c9132774d3548951097f26b8e2d040
Greg Look <greg@greg-look.net>
2014-03-31 21:40:06 -0700

    Bump snapshot version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-hiera "0.5.0"
+(defproject lein-hiera "0.6.0-SNAPSHOT"
   :description "Generates a dependency hierarchy graph for project namespaces."
   :url "https://github.com/greglook/lein-hiera"
   :license {:name "Public Domain"
