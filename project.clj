392bb6ece57cc4d312862e56971201d4e9f4c886
Greg Look <greg@greg-look.net>
2015-11-11 22:32:06 -0800

    Set release version 0.9.5.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-hiera "0.9.5-SNAPSHOT"
+(defproject lein-hiera "0.9.5"
   :description "Generates a dependency hierarchy graph for project namespaces."
   :url "https://github.com/greglook/lein-hiera"
   :license {:name "Public Domain"
